8add35d3b74659c20fd2a2ba747c10cc4e28baa1
yogesharma92 <yogesharma92@gmail.com>
2019-09-03 22:04:47 +0530

    Update DataApplication.java

--- src/main/java/DataApplication.java
+++ src/main/java/DataApplication.java
@@ -3,6 +3,7 @@ package testDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DataApplication {
 
