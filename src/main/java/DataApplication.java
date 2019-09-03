5e4cd46ca2ee32ed4377609e2f5defbfa80bf8a2
yogesharma92 <yogesharma92@gmail.com>
2019-09-03 18:49:08 +0530

    Create DataApplication.java

---
+++ src/main/java/DataApplication.java
@@ -0,0 +1,14 @@
+package testDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(DataApplication.class, args);
+	}
+
+}
