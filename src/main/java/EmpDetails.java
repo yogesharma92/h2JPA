810c86886c34e6b5216fe18942a38d79ee848f4c
yogesharma92 <yogesharma92@gmail.com>
2019-09-03 22:06:31 +0530

    Update EmpDetails.java

--- src/main/java/EmpDetails.java
+++ src/main/java/EmpDetails.java
@@ -1,14 +1,40 @@
 package testDatabase;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class EmpDetails {
 	
 	@Id
-	private long empId;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private long empId;
 	
 	private String empName;
+	
+	public long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public EmpDetails(String empName) {
+		super();
+		this.empName = empName;
+	}
+
+
 
 }
