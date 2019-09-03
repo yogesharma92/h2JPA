c2da8be75e36e4c2f15b032c444c2e586060c9b3
yogesharma92 <yogesharma92@gmail.com>
2019-09-03 22:05:08 +0530

    Update DataController.java

--- src/main/java/DataController.java
+++ src/main/java/DataController.java
@@ -1,18 +1,27 @@
 package testDatabase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 //@CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class DataController {
-
+	  
+	@Autowired
+	    private EmpRepository repository;
+    
 	
 	@RequestMapping(value = "/add", produces = "application/json", consumes = "application/json")
-    public String createPaymentCart(@RequestBody EmpDetails request) {
+    public String createPaymentCart(String res) {
     	System.out.println("inside");
+    	repository.save(new EmpDetails("Yogesh Sharma"));
+    	
+        
+        
     	return "Done" ;
     }
 }
